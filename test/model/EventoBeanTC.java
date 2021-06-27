7f91e73910874c6d97fadc0c0df599a4503d3dc7
sim1-avo <56638877+sim1-avo@users.noreply.github.com>
2021-06-27 13:38:08 +0200

    Modificati nomi Test Case

--- test/model/EventoBeanTC.java
+++ test/model/EventoBeanTC.java
@@ -17,10 +17,38 @@ class EventoBeanTC {
   }
 
   @Test
-  void testGetImmagine() {
+  void testSetImmagine() {
     EventoBean b = new EventoBean(1, "nome", "descrizione", null, "immagine", 
         new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()));
     b.setImmagine("ok");
     assertEquals("ok", b.getImmagine());
+  }
+  @Test
+  void testSetNome() {
+    EventoBean b = new EventoBean(1, "nome", "descrizione", null, "immagine",
+            new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()));
+    b.setNome("ok");
+    assertEquals("ok", b.getNome());
+  }
+  @Test
+  void testSetDescizione() {
+    EventoBean b = new EventoBean(1, "nome", "descrizione", null, "immagine",
+            new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()));
+    b.setDescrizione("ok");
+    assertEquals("ok", b.getDescrizione());
+  }
+  @Test
+  void testSetDataInizio() {
+    EventoBean b = new EventoBean(1, "nome", "descrizione", null, "immagine",
+            new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()));
+    b.setDataInizio(new Timestamp(System.currentTimeMillis()));
+    assertEquals(new Timestamp(System.currentTimeMillis()), b.getDataInizio());
+  }
+  @Test
+  void testSetDataFine() {
+    EventoBean b = new EventoBean(1, "nome", "descrizione", null, "immagine",
+            new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()));
+    b.setDataFine(new Timestamp(System.currentTimeMillis()));
+    assertEquals(new Timestamp(System.currentTimeMillis()), b.getDataFine());
   }
 }
